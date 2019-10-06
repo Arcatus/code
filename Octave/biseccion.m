9e92858cbf71918ae319516ac5151b2926a59ff8
Arcatus <starlightone@outlook.com>
2019-10-06 03:20:19 -0500

    06 10 19

--- Octave/biseccion.m
+++ Octave/biseccion.m
@@ -7,7 +7,8 @@ function z=biseccion()
  grid on;
  xl=input('Proporcione el valor inferior del intervalo, xl = ');
  xu=input('Proporcionel el valor superior del intervalo, xu = ');
- Es=input('Proporcione el valor de error de tolerancia, Es = ');
+ %Es=input('Proporcione el valor de error de tolerancia, Es = ');
+ n=input('Proporcione el numero de digitos significativos de precision del resultado, n = ')
  fxl=feval(funcion,xl);
  fxu=feval(funcion,xu);
  if fxl*fxu>0
@@ -15,6 +16,8 @@ function z=biseccion()
  else
   xr=0;
   ni=0;
+  Es = 0.5*10.^(2-n);
+  Ea = 100;
   do 
     xrant=xr; 
     xr=(xl+xu)/2;
@@ -24,11 +27,16 @@ function z=biseccion()
     elseif fxl*fxr>0
       xl=xr;
     endif
-    Ea=((xr-xrant)/xr)*100;
+    if ni > 1
+      Ea=((xr-xrant)/xr)*100;
+    endif
     ni++;
   until(abs(Ea)<Es);
  endif 
   z=double(xr);
   format long, z
-  printf('Se alcanzo el resultado final en %d iteraciones',ni); 
+  printf('El valor de la raiz es, %f\n',xr);
+  printf('El valor de la funcion en la raiz es: %f\n', fxr);
+  printf('El valor del error aproximado es %f\n', Ea);
+  printf('El numero de iteraciones es: %d\n', ni);
  
