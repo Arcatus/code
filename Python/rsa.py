a789f277d45d90b2cf2d76604dc83d152d281e6d
Arcatus <starlightone@outlook.com>
2019-10-20 05:31:39 -0500

    add python excercises

--- Python/rsa.py
+++ Python/rsa.py
@@ -2,23 +2,30 @@
 
 from Crypto.PublicKey import RSA
 
-def xgcd(a, b):
+def extended_euclid(a, b):
     if a == 0L:
         return (b, 0L, 1L)
     else:
-        g, x, y = xgcd(b%a, a)
+        g, x, y = extended_euclid(b%a, a)
         return (g, y - (b // a) * x, x)
 
 def modularInverse(a, b):
-    g, x, _ = xgcd(a, b)
+    # gdc, d, s: solo nos interesa d
+    g, x, _ = extended_euclid(a, b)
     if g == 1:
         return x % b
 
 def main():
     
-    n = 0x00d748e7389e3e2be7ebc931b7659e53cb
-    prim1 = 15892154504452115339L
-    prim2 = 18006529832677728449L
+    #n = 0x00d748e7389e3e2be7ebc931b7659e53cb
+    #prim1 = 15892154504452115339L
+    #prim2 = 18006529832677728449L
+
+    n = 0x00c262478a7171ed793af713edcc3003cb
+
+    prim1 = 14459458659007638593L
+    prim2 = 17869308484320582923L
+    
     e = 65537L
 
     d = modularInverse(e, ( prim1 - 1L ) * (prim2 - 1L) )
