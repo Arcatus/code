620e6882b4d6b48877a1746d0d118e93c767e691
Arcatus <starlightone@outlook.com>
2020-01-28 21:31:05 -0600

    28 enero 2020

---
+++ OmegaUp/invMat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, q;
+
+	cin >> n >> q;
+
+	char opc;
+	int columna, i1, i2;
+	
+	while(q--) {
+		cin >> columna >> i1 >> i2;
+
+		
+	}
+}
--- Python/redUno/utilidades/load_json_constructor.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def load_json_constructor(file_json):
-    with open(file_json) as json_variable:
-        data = json.loads(json_variable.read())
-    return data
